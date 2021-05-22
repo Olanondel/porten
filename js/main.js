f36bf24e973518558b9f9d4176a783f7e4cb5c37
Olanondel <prbaoleg@gmail.com>
2021-05-22 21:35:42 +0300

    new changes

--- js/main.js
+++ js/main.js
@@ -1,124 +1,144 @@
-$(
-    function () {
-        $('.new_season_card_list').slick({
-            slidesToShow: 1,
-            mobileFirst: true,
-            infinite: true,
-            autoplay: true,
-            autoplaySpeed: 3000,
-            speed: 1500,
-            arrows: false,
-            responsive: [
-                {
-                    breakpoint: 960,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                }
-            ]
-        })
-    }
-    )
+const imgBlock = document.querySelector('.img_block')
+const newCollection = document.querySelector('.new_collection')
 
-$(
-    function () {
-        $('.new_items_screen .card_list').slick({
-            infinite: true,
-            slidesToShow: 1,
-            autoplay: true,
-            autoplaySpeed: 3000,
-            speed: 1500,
-            arrows: false,
-            mobileFirst: true,
-            responsive: [
-                {
-                    breakpoint: 1100,
-                    settings: "unslick"
-                },
-                {
-                    breakpoint: 900,
-                    settings: {
-                        slidesToShow: 4,
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                    }
-                }
-            ]
-        })
-    }
-)
+// change Hight for imgBlock
+imgBlock.clientHeight = newCollection.clientHeight
 
-$(
-    function () {
-        $('.brand_list').slick({
-            infinite: true,
-            slidesToShow: 1,
-            autoplay: true,
-            autoplaySpeed: 3000,
-            speed: 1500,
-            arrows: false,
-            mobileFirst: true,
-            responsive: [
-                {
-                    breakpoint: 1100,
-                    settings: "unslick"
-                },
-                {
-                    breakpoint: 900,
-                    settings: {
-                        slidesToShow: 4,
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                    }
-                }
-            ]
-        })
-    }
-)
-
-$(function(){
-    var equal_height = 0;
-    $(".new_season_card_list .card_item").each(function(){
-        if ($(this).height() > equal_height) { equal_height = $(this).height(); }
-    });
-    $(".new_season_card_list .card_item").height(equal_height);
-});
-
-$(function(){
-    var equal_height = 0;
-    $(".new_season_card_list .card_item img").each(function(){
-        if ($(this).height() > equal_height) { equal_height = $(this).height(); }
-    });
-    $(".new_season_card_list .card_item img").height(equal_height);
-});
-
-$(function(){
-    var equal_height = 0;
-    $(".new_items_screen .card_item").each(function(){
-        if ($(this).height() > equal_height) { equal_height = $(this).height(); }
-    });
-    $(".new_season_card_list .card_item").height(equal_height);
+$(function () {
+  // slick for new season card
+  $('.new_season_card_list').slick({
+    slidesToShow: 1,
+    mobileFirst: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1500,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  })
 })
 
-$(function(){
-    var equal_height = 0;
-    $(".new_items_screen .card_item img").each(function(){
-        if ($(this).height() > equal_height) { equal_height = $(this).height(); }
-    });
-    $(".new_items_screen .card_item img").height(equal_height);
-});
+$(function () {
+  // slick for new_items_screen
+  $('.new_items_screen .card_list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1500,
+    arrows: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: 'unslick',
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  })
+})
 
+$(function () {
+  // slick for brand_list
+  $('.brand_list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1500,
+    arrows: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: 'unslick',
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  })
+})
+
+$(function () {
+  // align height all slider item for new_season_card_list
+  var equal_height = 0
+  $('.new_season_card_list .card_item').each(function () {
+    if ($(this).height() > equal_height) {
+      equal_height = $(this).height()
+    }
+  })
+  $('.new_season_card_list .card_item').height(equal_height)
+})
+
+$(function () {
+  // align height all slider item for new_season_card_list -> img
+  var equal_height = 0
+  $('.new_season_card_list .card_item img').each(function () {
+    if ($(this).height() > equal_height) {
+      equal_height = $(this).height()
+    }
+  })
+  $('.new_season_card_list .card_item img').height(equal_height)
+})
+
+$(function () {
+  // align height all slider item for new_items_screen .card_item
+  var equal_height = 0
+  $('.new_items_screen .card_item').each(function () {
+    if ($(this).height() > equal_height) {
+      equal_height = $(this).height()
+    }
+  })
+  $('.new_season_card_list .card_item').height(equal_height)
+})
+
+$(function () {
+  // align height all slider item for new_items_screen .card_item -> img
+  var equal_height = 0
+  $('.new_items_screen .card_item img').each(function () {
+    if ($(this).height() > equal_height) {
+      equal_height = $(this).height()
+    }
+  })
+  $('.new_items_screen .card_item img').height(equal_height)
+})
