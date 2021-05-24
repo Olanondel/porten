a42fb285e2ff8a5612b427205d75c96877be37a9
Olanondel <prbaoleg@gmail.com>
2021-05-24 22:09:04 +0300

    22:09

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
 $(document).ready(function () {
 
+    $(window).resize(function () { // $(window).width() < 769 &&
+
+        if (!$('.slider-2').hasClass('slick-initialized') && $(window).width() < 769) {
+            $('.new_items_screen .products_list').slick({
+                arrows: false,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                autoplay: true,
+                mobileFirst: true,
+                autoplaySpeed: 2000,
+                speed: 1500,
+                responsive: [
+                    {
+                        breakpoint: 769,
+                        settings: 'unslick'
+                    },
+                    {
+                        breakpoint: 520,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3
+                        }
+                    },
+                    {
+                        breakpoint: 277,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 100,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            })
+        }
+
+        if (!$('.slider-3').hasClass('slick-initialized') && $(window).width() < 620) {
+            $('.brand_items_list').slick({
+                arrows: false,
+                slidesToShow: 1,
+                autoplay: true,
+                mobileFirst: true,
+                autoplaySpeed: 2000,
+                speed: 1500,
+                responsive: [
+                    {
+                        breakpoint: 620,
+                        settings: 'unslick'
+                    }
+                ]
+            })
+        }
+
+    })
+
     $('.burger_btn').click(function (e) {
         e.preventDefault()
         $('.nav').slideToggle(450, function () {
